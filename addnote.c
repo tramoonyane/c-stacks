bb9c7bec573fb0db4be534d292960122afa3eb84
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 18:16:17 +0200

    Update addnote.c

--- addnote.c
+++ addnote.c
@@ -1,9 +1,15 @@
 #include "monty.h"
 /**
- * addnode - add node to the head stack
- * @head: head of the stack
- * @n: new_value
- * Return: no return
+ * addnode - Adds a node to the top of the stack
+ * @head: Double pointer to the head of the stack
+ * @n: Value to be added to the stack
+ *
+ * Description: Creates a new node with the given value @n and adds it to the top
+ * of the stack. If memory allocation fails for the new node, it prints an error
+ * message "Error" to stdout and exits with status 0.
+ *
+ * @head: Pointer to the head pointer of the stack
+ * @n: Value to be added to the stack
  */
 void addnode(stack_t **head, int n)
 {
