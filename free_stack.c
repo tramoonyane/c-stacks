daf86ce987974b91113ebd175fbbdfa341714b2c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 18:14:32 +0200

    Update free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -1,7 +1,13 @@
 #include "monty.h"
 /**
- * free_stack - frees a doubly linked list
- * @head: head of the stack
+ * free_stack - Frees a doubly linked list representing a stack
+ * @head: Pointer to the head of the stack
+ *
+ * Description: Frees all the nodes in a doubly linked list
+ * representing a stack. This function releases the memory
+ * occupied by each node in the stack.
+ *
+ * @head: Pointer to the head of the stack
  */
 void free_stack(stack_t *head)
 {
