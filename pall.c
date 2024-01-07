b9d76398fc5beacccbe4ca4077b94d1602b1aa52
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 17:17:08 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -7,15 +7,15 @@
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-  stack_t *h;
-  (void)counter;
+stack_t *h;
+(void)counter;
 
-  h = *head;
-  if (h == NULL)
-    return;
-  while (h)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
+h = *head;
+if (h == NULL)
+return;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
 }
