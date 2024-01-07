6e25a7eadf4dab25e52fc46c8bfd2fbe1e4edb62
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 18:09:21 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,9 +1,15 @@
 #include "monty.h"
 /**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
+ * f_pall - Prints all values in the stack
+ * @head: Double pointer to the head of the stack
+ * @counter: Line number counter for potential error messages (unused)
+ *
+ * Description: Prints all the values in the stack. If the stack is empty,
+ * it returns without printing anything. This function assumes a doubly
+ * linked list representation of the stack.
+ *
+ * @head: Pointer to the head pointer of the stack
+ * @counter: Line number (unused in this function)
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
