3598aca5c0b83fb65a46b969dcb4698ea1d8944f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 18:05:34 +0200

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,9 +1,16 @@
 #include "monty.h"
 /**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_pop - Removes the top element of the stack
+ * @head: Double pointer to the head of the stack
+ * @counter: Line number counter for potential error messages
+ *
+ * Description: Removes the top element of the stack. If the stack is empty,
+ * it prints an error message "L<counter>: can't pop an empty stack" to stderr
+ * and exits with EXIT_FAILURE. This function assumes a doubly linked list
+ * representation of the stack.
+ *
+ * @head: Pointer to the head pointer of the stack
+ * @counter: Line number from the bytecode file for error tracking
  */
 void f_pop(stack_t **head, unsigned int counter)
 {
