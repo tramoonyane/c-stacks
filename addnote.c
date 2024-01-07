2993ae0347c9c7100ed0cdeb9d90edfb293e59bc
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 18:24:15 +0200

    Update addnote.c

--- addnote.c
+++ addnote.c
@@ -4,8 +4,10 @@
  * @head: Double pointer to the head of the stack
  * @n: Value to be added to the stack
  *
- * Description: Creates a new node with the given value @n and adds it to the top
- * of the stack. If memory allocation fails for the new node, it prints an error
+ * Description: Creates a new node with the given value
+ * @n and adds it to the top
+ * of the stack. If memory allocation fails for the new node,
+ * it prints an error
  * message "Error" to stdout and exits with status 0.
  *
  * @head: Pointer to the head pointer of the stack
