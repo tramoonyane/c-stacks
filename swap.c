84a549882c85d1d91df85da2d4c0886e16a9d092
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 17:48:43 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,9 +1,16 @@
 #include "monty.h"
 /**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_swap - Swaps the top two elements of the stack
+ * @head: Double pointer to the head of the stack
+ * @counter: Line number counter for error messages
+ *
+ * Description: Swaps the top two elements of the stack.
+ * If the stack contains less than two elements, prints an error message
+ * "L<counter>: can't swap, stack too short" to stderr and exits with EXIT_FAILURE.
+ * This function assumes a doubly linked list representation of the stack.
+ *
+ * @head: Pointer to the head pointer of the stack
+ * @counter: Line number from the bytecode file for error tracking
  */
 void f_swap(stack_t **head, unsigned int counter)
 {
