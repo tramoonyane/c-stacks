0e94de7fa0d62d478ef5a0e97535c8e54c57d3cb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-07 17:57:36 +0200

    Update rotl.c

--- rotl.c
+++ rotl.c
@@ -1,9 +1,16 @@
 #include "monty.h"
 /**
- *f_rotl- rotates the stack to the top
- *@head: stack head
- *@counter: line_number
- *Return: no return
+ * f_rotl - Rotates the stack to the top
+ * @head: Double pointer to the head of the stack
+ * @counter: Line number counter for potential error messages (unused)
+ *
+ * Description: Rotates the stack to the top, placing the bottom element
+ * at the top of the stack. If the stack contains fewer than two elements
+ * or is empty, it returns without performing any rotation.
+ * This function assumes a doubly linked list representation of the stack.
+ *
+ * @head: Pointer to the head pointer of the stack
+ * @counter: Line number (unused in this function)
  */
 void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 {
